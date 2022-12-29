ec17358ed8481da06fdd09c5098c9e7454f860cd
KIM_SEJIN05 <kshn0303@naver.com>
2022-12-29 14:00:26 +0900

    메인화면작업

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -10,6 +10,23 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    return PageView(
+      children: [
+        _Question("우리가 연애를 시작한 날", "같이 갔던 술집 이름은?"),
+        _Question("우리가 연애를 시작한", "정확한 날짜는?"),
+      ],
+    );
+  }
+}
+
+class _Question extends StatelessWidget {
+  String Q;
+  String Q_1;
+
+  _Question(this.Q, this.Q_1);
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         image: DecorationImage(
@@ -22,70 +39,59 @@ class _HomeScreenState extends State<HomeScreen> {
         body: SafeArea(
           child: Container(
             width: MediaQuery.of(context).size.width,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                _Question(),
-              ],
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 180,
+                    color: Colors.white,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                          children: [
+                            Text(Q,
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.w700)),
+                            Text(Q_1,
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.w700)),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: TextFormField(
+                            decoration:
+                                new InputDecoration(hintText: "정답을 입력하세요."),
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        alignment: Alignment.center,
+                        fixedSize: Size(300, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        )),
+                    onPressed: () {},
+                    child: Text(
+                      "다음단계로 이동",
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
-      ),
-    );
-  }
-}
-
-class _Question extends StatelessWidget {
-  const _Question({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        children: [
-          Container(
-            height: 180,
-            color: Colors.white,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    Text("우리가 연애를 시작한 날,",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.w700)),
-                    Text("같이 갔던 술집 이름은?!",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.w700)),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: TextFormField(
-                    decoration: new InputDecoration(hintText: "정답을 입력하세요."),
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              alignment: Alignment.center,
-              fixedSize: Size(300, 50),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0),
-              )
-            ),
-            onPressed: () {},
-            child: Text("다음단계로 이동",
-            style: TextStyle(fontSize: 25),),
-          ),
-        ],
       ),
     );
   }
