7389874fb25a68e2f2762395de9e2c77c6a3ed8c
SEJIN <kshn0303@naver.com>
2023-01-03 14:12:11 +0900

    슬라이더 기능추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:marry_project/screen/photo_setting.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,91 +10,40 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final List<int> imageOrder = [0, 1, 2, 3, 4];
+
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      children: [
-        _Question("우리가 연애를 시작한 날", "같이 갔던 술집 이름은?"),
-        _Question("우리가 연애를 시작한", "정확한 날짜는?"),
-      ],
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                      builder: (BuildContext context) {
+                        return PhotoSetting();
+                      }
+                  ),
+                );
+              },
+              icon: Icon(Icons.settings),
+            ),
+            ChoicePhoto(),
+          ],
+        ),
+      ),
     );
   }
 }
 
-class _Question extends StatelessWidget {
-  String Q;
-  String Q_1;
-
-  _Question(this.Q, this.Q_1);
+class ChoicePhoto extends StatelessWidget {
+  const ChoicePhoto({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          fit: BoxFit.cover,
-          image: AssetImage('assets/main_photo.jpeg'),
-        ),
-      ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: SafeArea(
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 180,
-                    color: Colors.white,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Column(
-                          children: [
-                            Text(Q,
-                                style: TextStyle(
-                                    fontSize: 20, fontWeight: FontWeight.w700)),
-                            Text(Q_1,
-                                style: TextStyle(
-                                    fontSize: 20, fontWeight: FontWeight.w700)),
-                          ],
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: TextFormField(
-                            decoration:
-                                new InputDecoration(hintText: "정답을 입력하세요."),
-                            style: TextStyle(fontSize: 20),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        alignment: Alignment.center,
-                        fixedSize: Size(300, 50),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                        )),
-                    onPressed: () {},
-                    child: Text(
-                      "다음단계로 이동",
-                      style: TextStyle(fontSize: 25),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
+    return Expanded(child: Image.asset('assets/1.jpeg'));
   }
 }
---
+++ lib/screen/photo_setting.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class PhotoSetting extends StatefulWidget {
+  const PhotoSetting({Key? key}) : super(key: key);
+
+  @override
+  State<PhotoSetting> createState() => _PhotoSettingState();
+}
+
+class _PhotoSettingState extends State<PhotoSetting> {
+  double photoOrder = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            PhotoShow(photoOrder: photoOrder.toInt()),
+            Slider(
+                value: photoOrder,
+                max: 1,
+                min: 0,
+                onChanged: (double val) {
+                  setState(() {
+                    photoOrder = val;
+                  });
+                }
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PhotoShow extends StatelessWidget {
+  final photoOrder ;
+
+  const PhotoShow({
+    required this.photoOrder, Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: photoOrder.toString().split('').map((x) => Image.asset('assets/$x.jpeg'))
+      .toList(),
+    );
+  }
+}
